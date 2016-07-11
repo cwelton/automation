c6f64b24d387c48526436cbdec03bc124d5074a5
Caleb Welton <cwelton@apache.org>
2016-07-11 13:16:57 -0700

    add support for google test based unit tests

---
+++ cmake/Modules/googletest.cmake
@@ -0,0 +1,22 @@
+set(GTEST_VERSION "1.7.0")
+set(GTEST_ZIP "gtest-${GTEST_VERSION}.zip")
+set(GTEST_URL "http://googletest.googlecode.com/files/${GTEST_ZIP}")
+set(GTEST_MD5 "2d6ec8ccdf5c46b05ba54a9fd1d130d7")
+
+ExternalProject_Add(
+  googletest
+  PREFIX ${AUTOMATION_THIRD_PARTY}
+  DOWNLOAD_DIR ${AUTOMATION_THIRD_PARTY}/downloads
+  URL ${GTEST_URL}
+  URL_MD5 ${GTEST_MD5}
+  TIMEOUT 10
+  INSTALL_COMMAND ""
+  PATCH_COMMAND ""
+  UPDATE_COMMAND ""
+  # Wrap download, configure and build steps in a script to log output
+  LOG_DOWNLOAD OFF
+  LOG_CONFIGURE OFF
+  LOG_BUILD OFF)
+
+include_directories( SYSTEM ${AUTOMATION_THIRD_PARTY}/src/googletest/include )
+link_directories( ${AUTOMATION_THIRD_PARTY}/src/googletest-build )
---
+++ test/hello_test.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "gtest/gtest.h"
+
+namespace AutomationTest {
+
+class TestFixture : public ::testing::Test {
+
+protected:
+	TestFixture() = default;
+	virtual ~TestFixture() = default;
+	virtual void SetUp() { }
+	virtual void TearDown() {}
+
+};
+
+TEST_F(TestFixture, ExampleTest) {
+	int i = 0;
+    EXPECT_EQ(0, i);
+}
+
+} // namespace AutomationTest
