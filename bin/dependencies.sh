34bcf7c27a3ad0eec9e879f775ceb0c053076717
Caleb Welton <cwelton@apache.org>
2016-07-28 17:04:54 -0700

    updates bin/dependencies in attempt to fix travis

--- bin/dependencies.sh
+++ bin/dependencies.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 apt-get update
 apt-get install -y build-essential cmake lcov
@@ -7,6 +8,6 @@ apt-get install -y python python-dev python-pip
 pip install --upgrade pip
 pip install gcovr
 
-wget --quiet https://github.com/oclint/oclint/releases/download/v0.10.3/oclint-0.10.3-x86_64-linux-3.13.0-74-generic.tar.gz -O /tmp/oclint-0.10.3.tar.gz
+wget --quiet http://github.com/oclint/oclint/releases/download/v0.10.3/oclint-0.10.3-x86_64-linux-3.13.0-74-generic.tar.gz -O /tmp/oclint-0.10.3.tar.gz
 tar -xzf /tmp/oclint-0.10.3.tar.gz
 ln -s oclint-0.10.3 oclint
